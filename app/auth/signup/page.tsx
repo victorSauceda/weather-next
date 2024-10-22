2ea8bbe151aa590edde08c2c74ec54569f0872d6
victorSauceda <victorasauceda@gmail.com>
2024-10-22 15:36:57 -0500

    updates

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -21,7 +21,7 @@ export default function SignUp() {
       return;
     }
     try {
-      const res = await fetch('/api/auth/signup', {
+      const res = await fetch('/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
