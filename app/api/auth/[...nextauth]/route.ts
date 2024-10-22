49a34d432c362b555aa08190f3c9e018ba618d38
victorSauceda <victorasauceda@gmail.com>
2024-10-22 11:54:06 -0500

    updates

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,6 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth'
+import authOptions from '../../../../lib/auth'
 
-const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    })
-  ],
-  pages: {
-    signIn: '/auth/signin', // Customize your sign-in page route
-  }
-};
+const handler = NextAuth(authOptions)
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+  },
+  secret: process.env.SECRET
+};
+
+export default authOptions;
