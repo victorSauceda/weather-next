ef3824987b450a1bfc283d3bb947b88845dfaa5c
victorSauceda <victorasauceda@gmail.com>
2024-10-24 12:30:49 -0500

    updates

--- app/api/user/add-city/route.ts
+++ app/api/user/add-city/route.ts
@@ -8,23 +8,41 @@ import { City as CityType } from '../../../components/AutocompleteSearch'; // Im
 import authOptions from '../../../../lib/auth';
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  console.log("test route")
-  await dbConnect();
-  const body = await req.json()
-  const { city }: { city: CityType } = body;
-  console.log(city);
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({message: 'unauthorized'}, {status: 401});
+  try {
+    console.log("test route");
 
-  const user = await User.findOne({ email: session.user?.email });
-  if (!user) return NextResponse.json({message: 'User Not Found'},{status: 404})
+    // Connect to the database
+    await dbConnect();
 
-  // Add city to favorites
-  let cityRecord = await City.findOne({ id: city.id });
-  if (!cityRecord) {
-    cityRecord = await City.create(city);
+    // Parse request body
+    const body = await req.json();
+    const { city }: { city: CityType } = body;
+    console.log(city);
+
+    // Get session
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ message: 'unauthorized' }, { status: 401 });
+    }
+
+    // Find the user by email
+    const user = await User.findOne({ email: session.user?.email });
+    if (!user) {
+      return NextResponse.json({ message: 'User Not Found' }, { status: 404 });
+    }
+
+    // Add city to favorites
+    let cityRecord = await City.findOne({ id: city.id });
+    if (!cityRecord) {
+      cityRecord = await City.create(city);
+    }
+    user.favoriteCities.push(cityRecord._id);
+    await user.save();
+
+    return NextResponse.json(user.favoriteCities, { status: 200 });
+    
+  } catch (error) {
+    console.error('Error in addFavorite route:', error);
+    return NextResponse.json({ message: 'Server error' }, { status: 500 });
   }
-  user.favoriteCities.push(cityRecord._id);
-  await user.save();
-  return NextResponse.json(user.favoriteCities, {status: 200});
 }
