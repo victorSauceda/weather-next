49a02783b245bd242f6b764276fc992931d2b249
victorSauceda <victorasauceda@gmail.com>
2024-10-24 14:00:19 -0500

    updates

--- models/City.ts
+++ models/City.ts
@@ -1,5 +1,4 @@
-
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 
 export interface ICity extends Document {
   id: number;
@@ -12,15 +11,17 @@ export interface ICity extends Document {
   };
 }
 
-const CitySchema: Schema = new Schema({
+const CitySchema: Schema<ICity> = new Schema({
   id: { type: Number, required: true },
   name: { type: String, required: true },
   state: { type: String },
   country: { type: String, required: true },
   coord: {
     lon: { type: Number, required: true },
-    lat: { type: Number, required: true }
-  }
+    lat: { type: Number, required: true },
+  },
 });
 
-export default mongoose.models.City || mongoose.model<ICity>('City', CitySchema);
+const City: Model<ICity> = mongoose.models.City || mongoose.model<ICity>('City', CitySchema);
+
+export default City;
